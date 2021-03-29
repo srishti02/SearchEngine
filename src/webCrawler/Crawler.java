929d95b24716790f9a5635aac77185f2c64f03fa
Srishti Jain <jain71@uwindsor.ca>
2021-03-29 19:08:41 -0400

    Crawler 1000 limit applied and hashmap logic added

--- src/webCrawler/Crawler.java
+++ src/webCrawler/Crawler.java
@@ -1,6 +1,9 @@
 package webCrawler;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -12,6 +15,8 @@ import java.io.*;
 public class Crawler {
 
   private HashSet<String> urls = new HashSet<String>();
+  private HashMap<String, List<String>> urlLinks = 
+    new HashMap<String, List<String>>();
 
   /**
    * @brief Method to crawl the url passed as argument and get all the
@@ -25,7 +30,7 @@ public class Crawler {
       return;
 
     /** check if the url has already been crawled*/
-    if(!urls.contains(referenceUrl))
+    if(!urls.contains(referenceUrl) && urls.size() < 1000)
     {
       /** add url to crawled urls set*/
       urls.add(referenceUrl);
@@ -42,6 +47,9 @@ public class Crawler {
         /** for all urls fetched from page*/
         for(Element url : urlsToCrawl)
         {
+          /** add to reference hash map*/
+          addToReferenceLinks(referenceUrl,url.attr("abs:href"));
+
           /** recursive call*/
           crawl(url.attr("abs:href"));
         }
@@ -49,6 +57,17 @@ public class Crawler {
       catch (IOException e) 
       {}
     }
+  }
+
+  public HashMap<String, List<String>> getReferencedLinksMap()
+  {
+    return urlLinks;
+  }
+
+  private void addToReferenceLinks(String key, String value)
+  {
+    urlLinks.putIfAbsent(key, new ArrayList<String>());
+    urlLinks.get(key).add(value);
   }
 
   public static void main(String[] args)
